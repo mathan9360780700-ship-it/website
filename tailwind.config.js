1c7ecd8db9767f5a336becb55bf85e3123ee4af0
Mathan <mathan9360780700@gmail.com>
2026-03-07 12:12:07 +0530

    configure the taliwind js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/**  @type {import('@tailwindcss/cli').config}*/
+MediaSourceHandle.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            
+        }
+    },
+    Plugin: [],
+}
